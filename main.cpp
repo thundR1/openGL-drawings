d565bd6e2e8ed18fcca23fedcf90f443f23db7de
Youssef Amin <88247258+thundR1@users.noreply.github.com>
2023-03-16 21:01:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+
+void disp(void) {
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLE_FAN);
+        glColor3f(177.0/255, 37.0/255, 234.0/255);
+        glVertex2f(-0.5, 0.5);
+        glColor3f(226.0/255, 68.0/255, 98.0/255);
+        glVertex2f(0.5, 0.5);
+        glColor3f(127.0/255, 82.0/255, 255.0/255);
+        glVertex2f(-0.5, -0.5);
+        glColor3f(0, 0, 0);
+        glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowPosition(470, 150);
+    glutInitWindowSize(512, 512);
+    glutCreateWindow("My Window");
+
+    glutDisplayFunc(disp);
+    glutMainLoop();
+    return 0;
+}
